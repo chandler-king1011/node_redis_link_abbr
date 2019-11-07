87e9638ae46411ab558ea4845e434d57ccf661c2
Chandler King <ckmotox356@gmail.com>
2019-11-06 18:28:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node/Redis Link Shortener
+
+This is a simple web app built using nodejs that takes in a link and returns a shortened link with the use of Redis. This was built to become more familiar with how Redis works and how I could connect it to an application. 
+
---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+const fs = require('fs');
+const exphbs = require("express-handlebars");
+
+const port = 3000;
+const app = express();
+
+//view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+const hbs = exphbs.create({
+    helpers: {
+        links: " "
+        } ,
+    }
+);
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// redis client
+const client = redis.createClient();
+
+client.on('connect', function() {
+    console.log("Connected to Redis...");
+
+});
+
+client.on("error", function (err) {
+    console.log("Error connecting to Redis..." + err);
+});
+
+
+
+
+app.get("/", function(req, res, next) {
+    res.render("insertlink");
+    }
+);
+
+
+function sendLinks(obj) {
+    for (key in obj) {
+       return(obj[key]);
+    }
+}
+
+
+
+app.get("/links", function(req, response, next) {
+    client.hgetall("link", function(err, res) {
+        linkObj = res;
+        response.render("links", {links: sendLinks(linkObj)})
+     });
+});
+    
+
+
+
+
+
+
+app.post("/", function(req, res) {
+    let key ="link" 
+    let link = req.body.url
+    client.hmset(key, req.body.id, link);
+    client.hmget(key, req.body.id, function(err, response) {
+        let url = response;
+        res.render("insertlink", {
+            link: url
+        });
+    });
+});
+
+
+app.listen(port, function() {
+    console.log('Server started on port:' + port);
+});
+
+
+
+
+
+ 
+
+
+
